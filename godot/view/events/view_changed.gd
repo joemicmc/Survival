712b40c69aa5db53b17e791167c06e4f50a1efdb
joemicmc <joe@8bitape.net>
2023-05-12 15:31:01 +0100

    Reworked view service

---
+++ godot/view/events/view_changed.gd
@@ -0,0 +1,8 @@
+class_name ViewChanged
+extends ViewEvent
+
+var view: View
+
+
+func _init(_view: View) -> void:
+	view = _view
--- godot/view/view_service.gd
+++ godot/view/view_service.gd
@@ -1,9 +1,10 @@
-extends Node
+extends EventNode
 
 var stack: Array[View] = []
 
 
 func _ready() -> void:
+	super._ready()
 	push_root(Menu.instance())
 
 
@@ -11,20 +12,35 @@ func push_root(view: View) -> void:
 	stack.clear()
 	for child in get_children():
 		child.queue_free()
-	add_child(view)
-	stack.push_front(view)
-	stack.front().enter()
+	push_view(view)
 
 
-func push_modal(view: View) -> void:
-	stack.front().exit()
-	add_child(view)
-	stack.push_front(view)
-	stack.front().enter()
+func push_modal(modal: Modal) -> void:
+	push_view(modal)
 
 
 func pop_modal() -> void:
+	pop_view()
+
+
+func push_view(view: View) -> void:
+	if stack.size():
+		stack.front().exit()
+	add_child(view)
+	stack.push_front(view)
+	stack.front().enter()
+	
+	emit(ViewChanged.new(stack.front()))
+
+
+func pop_view() -> void:
+	if not stack.size():
+		return
 	stack.front().exit()
 	stack.front().queue_free()
 	stack.pop_front()
+	if not stack.size():
+		return
 	stack.front().enter()
+	
+	emit(ViewChanged.new(stack.front()))
