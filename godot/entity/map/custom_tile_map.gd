89c28ed898b6b51ca9a4b186eaf0c9329b97042c
joemicmc <joe@8bitape.net>
2023-06-22 20:50:59 +0100

    Fix warnings

--- godot/entity/map/custom_tile_map.gd
+++ godot/entity/map/custom_tile_map.gd
@@ -2,8 +2,8 @@
 class_name CustomTileMap
 extends TileMap
 
-func _use_tile_data_runtime_update (layer: int, coords: Vector2i) -> bool:
+func _use_tile_data_runtime_update (_layer: int, _coords: Vector2i) -> bool:
 	return true
 
-func _tile_data_runtime_update (layer: int, coords: Vector2i, tile_data: TileData) -> void:
+func _tile_data_runtime_update (_layer: int, _coords: Vector2i, _tile_data: TileData) -> void:
 	pass
